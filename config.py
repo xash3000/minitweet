c27ef66e0191a9762e80b5154365223817884ac6
ali <alifaki077@gmail.com>
2015-12-30 16:35:27 +0300

    database fixed

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 class BaseConfig(object):
     """
     Base Configuration which other configs inherits from it
     """
     DEBUG = False
     SECRET_KEY = b'5\x00\x05\x84\x8a3-\\K#\xba\x08\xf6"\x9b\xef3\x84\xcd\xc7'
-    SQLALCHEMY_DATABASE_URI = "postgresql:///minitweet_dev"
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
--- minitweet_app/__init__.py
+++ minitweet_app/__init__.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 app.config.from_object(os.environ["CONFIG"])
 db = SQLAlchemy(app)
-db.create_all()
+
 
 from . import views
