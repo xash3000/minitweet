2a8d4ebf64994ad1e1a47320f9d0be9c27ee405e
ali <alifaki077@gmail.com>
2015-12-31 18:06:49 +0300

    use properties instead of methods in user model

--- minitweet_app/models.py
+++ minitweet_app/models.py
@@ -35,15 +35,19 @@ class User(db.Model):
         # Encrypt password
         self.password = bcrypt.generate_password_hash(password)
 
+    @property
     def is_authenticated(self):
         return True
 
+    @property
     def is_active(self):
         return True
 
+    @property
     def is_anonymous(self):
         return False
 
+    @property
     def get_id(self):
         return str(self.id)
 
