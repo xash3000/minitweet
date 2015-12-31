896316aea9e92bae51df48705d6be45ff379cb18
ali <alifaki077@gmail.com>
2015-12-31 15:45:49 +0300

    add new validators to publish form

--- minitweet_app/__init__.py
+++ minitweet_app/__init__.py
@@ -1,22 +1,26 @@
 from flask import Flask
 import os
+
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
-
 app.config.from_object(os.environ["CONFIG"])
+
 db = SQLAlchemy(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
 login_manager.login_view = "views.login"
+
+bcrypt = Bcrypt(app)
+
 
 from .models import User
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.filter(User.id == int(user_id)).first()
+
 from . import views
--- minitweet_app/forms.py
+++ minitweet_app/forms.py
@@ -4,8 +4,12 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class PublishForm(Form):
-    post_title = StringField('post_title', validators=[DataRequired()])
-    textarea = TextAreaField("textarea", validators=[DataRequired()])
+    post_title = StringField(
+            'post_title', validators=[DataRequired(), Length(min=10, max=50)]
+    )
+    textarea = TextAreaField(
+            "textarea", validators=[DataRequired(), Length(min=3, max=300)]
+    )
 
 
 class LoginForm(Form):
