f41d921d2cbbf81b71191233ce2cf8488f4cc3cd
ali <alifaki077@gmail.com>
2016-01-04 17:22:32 +0300

    add better forms erros

--- minitweet_app/forms.py
+++ minitweet_app/forms.py
@@ -24,15 +24,9 @@ class SignUpForm(Form):
     )
     email = StringField(
         'email',
-        validators=[DataRequired(), Email(message=None), Length(min=6, max=40)]
+        validators=[DataRequired(), Email(), Length(min=5)]
     )
     password = PasswordField(
         'password',
         validators=[DataRequired(), Length(min=6, max=25)]
-    )
-    confirm = PasswordField(
-        'Repeat password',
-        validators=[
-            DataRequired(), EqualTo('password', message='Passwords must match.')
-        ]
     )
