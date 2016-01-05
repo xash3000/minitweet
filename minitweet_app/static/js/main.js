9d48972c2f705e88c2c9d862689039bec9960f45
ali <alifaki077@gmail.com>
2016-01-05 16:34:34 +0300

    make likes counting

--- minitweet_app/static/js/main.js
+++ minitweet_app/static/js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
     $(".heart").click(function() {
+            var original = +$(this).parent().find(".likes-counting").html();
             if($(this).hasClass("fa-heart-o")){
                 $(this).removeClass("fa-heart-o");
                 $(this).addClass("fa-heart fa-3x");
+                $(this).parent().find(".likes-counting").html(original + 1);
             } else {
                 $(this).removeClass("fa-heart fa-3x");
                 $(this).addClass("fa-heart-o");
+                $(this).parent().find(".likes-counting").html(original - 1);
             }
     })
 })
